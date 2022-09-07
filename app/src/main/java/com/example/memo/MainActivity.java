9adb067e1bb0e9b241e1f4dd9aa2657f75188e69
Deepak Singh <deepaksingh052003@gmail.com>
2022-09-07 08:18:34 +0530

    Completed Working app

--- app/src/main/java/com/example/memo/MainActivity.java
+++ app/src/main/java/com/example/memo/MainActivity.java
@@ -1,70 +1,97 @@
 package com.example.memo;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView img= findViewById(R.id.img);
+    ImageView img;
+    LottieAnimationView progress;
+ public String cururl=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        loadmeme();
+        loadmeme();
     }
 
-//    private void loadmeme(){
-//
-//        RequestQueue queue = Volley.newRequestQueue(this);
-//        String url = "https://meme-api.herokuapp.com/gimme";
-//
-//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-//                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-//
-//                    @Override
-//                    public void onResponse(JSONObject response) {
-//                        try {
-//                            String url= response.getString("url");
-//                            Glide.with(getApplicationContext()).load(url).into(img);
-//                        } catch (JSONException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                }, new Response.ErrorListener() {
-//
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                         TODO: Handle error
-//
-//                    }
-//                });
-//
-//// Add the request to the RequestQueue.
-//        queue.add(jsonObjectRequest);
-//    }
+    private void loadmeme(){
+
+        progress=findViewById(R.id.progress);
+        img= findViewById(R.id.img);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://meme-api.herokuapp.com/gimme";
+        progress.setVisibility(View.VISIBLE);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String url= response.getString("url");
+                            cururl=url;
+                            Glide.with(getApplicationContext()).load(url).listener(new RequestListener<Drawable>() {
+                                @Override
+                                public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                                    return false;
+                                }
+
+                                @Override
+                                public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                                    progress.setVisibility(View.GONE);
+                                    return false;
+                                }
+                            }).into(img);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                        Toast.makeText(MainActivity.this, "Some error occured !", Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+        queue.add(jsonObjectRequest);
+    }
 
     public void sharememe(View view) {
-        Toast.makeText(this, "Clicked on sharememe", Toast.LENGTH_SHORT).show();
+        Intent intent= new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT,cururl);
+
+        startActivity(intent);
     }
 
     public void nextmeme(View view) {
-        Toast.makeText(this, "Clicked on nextmeme", Toast.LENGTH_SHORT).show();
+        loadmeme();
 
     }
 }
